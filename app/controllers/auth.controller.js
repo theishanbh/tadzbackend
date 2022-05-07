3f41a8efaa5019196e6acfa053a236d2b55f8ffa
theishanbh <theishanbh@gmail.com>
2022-05-08 00:50:38 +0530

    done

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 const config = require("../config/auth.config");
 const db = require("../models");
 const User = db.user;
-const Product = require('../models/product.model')
+const Product = require("../models/product.model");
 
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
@@ -12,9 +12,8 @@ exports.signup = (req, res) => {
     email: req.body.email,
     password: bcrypt.hashSync(req.body.password, 8),
     phoneNo: req.body.phoneNo,
-    address: req.body.address
+    address: req.body.address,
   });
-
 
   user.save((err, user) => {
     if (err) {
@@ -23,14 +22,12 @@ exports.signup = (req, res) => {
     }
 
     res.send({ message: "User was registered successfully!" });
-
   });
-
 };
 
 exports.signin = (req, res) => {
   User.findOne({
-    username: req.body.username
+    username: req.body.username,
   })
     .populate("roles", "-__v")
     .exec((err, user) => {
@@ -51,12 +48,12 @@ exports.signin = (req, res) => {
       if (!passwordIsValid) {
         return res.status(401).send({
           accessToken: null,
-          message: "Invalid Password!"
+          message: "Invalid Password!",
         });
       }
 
       var token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 86400 // 24 hours
+        expiresIn: 86400, // 24 hours
       });
 
       res.status(200).send({
@@ -66,14 +63,14 @@ exports.signin = (req, res) => {
         phoneNo: user.phoneNo,
         // roles: authorities,
         address: user.address,
-        accessToken: token
+        accessToken: token,
       });
     });
 };
 
 exports.adminSignIn = (req, res) => {
   User.findOne({
-    username: 'admin'
+    username: "admin",
   })
     .populate("roles", "_v")
     .exec((err, user) => {
@@ -94,12 +91,12 @@ exports.adminSignIn = (req, res) => {
       if (!passwordIsValid) {
         return res.status(401).send({
           accessToken: null,
-          message: "Invalid Password!"
+          message: "Invalid Password!",
         });
       }
 
       var token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 86400 // 24 hours
+        expiresIn: 86400, // 24 hours
       });
 
       res.status(200).send({
@@ -107,15 +104,15 @@ exports.adminSignIn = (req, res) => {
         username: user.username,
         email: user.email,
         phoneNo: user.phoneNo,
-        roles: 'admin',
+        roles: "admin",
         address: user.address,
-        accessToken: token
+        accessToken: token,
       });
     });
 };
 
 exports.update = async (req, res) => {
-  const product = await Product.find({prodId:"iPhone 111001200"});
+  const product = await Product.find({ prodId: "iPhone 111001200" });
   console.log(product);
   // console.log(req.body.userFrom);
   // if (product) {
